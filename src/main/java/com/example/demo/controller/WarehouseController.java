abd732967470a4e1529c1cb2e8520274da600ff4
pvhung05 <phamvanhung242005@gmail.com>
2024-12-13 13:10:04 +0700

    ooo

--- src/main/java/com/example/demo/controller/WarehouseController.java
+++ src/main/java/com/example/demo/controller/WarehouseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://backend-awim.onrender.com")
 @RestController
 @RequestMapping("/api/warehouse")
 public class WarehouseController {
--- src/main/java/com/example/demo/controller/WarehouseHasProductController.java
+++ src/main/java/com/example/demo/controller/WarehouseHasProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://backend-awim.onrender.com")
 @RestController
 @RequestMapping("/api/warehousehasproduct")
 public class WarehouseHasProductController {
